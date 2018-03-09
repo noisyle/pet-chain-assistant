1963bbb60cb97526bb89b5a1c379268725261e27
York <anyone.1984@gmail.com>
2018-03-10 00:23:34 +0800

    add a timer

--- content_script.js
+++ content_script.js
@@ -7,9 +7,23 @@ $(function(){
             $(".sms-input input").keydown(function(e){
                 if(e.keyCode==13){
                     btn.click();
+                    setTimeout(function(){
+                        $("div.button").click();
+                        setTimeout(function(){
+                            $(".sms-input input")[0].focus();
+                        }, 1000);
+                    }, 500);
                 }
             });
         }, 1000);
-        $("title").text($(".tag-info .rareDegree").text() + " - " + $("title").text());
+
+        var rare = $(".tag-info .rareDegree").text();
+        var title = $("title").text();
+        var timeout = 180;
+        var timer = setInterval(function(){
+            timeout-=1;
+            $("title").text(rare + " : " + timeout + " - " + title);
+            if(timeout<=0) clearInterval(timer)
+        }, 1000);
     }, 200);
 });
